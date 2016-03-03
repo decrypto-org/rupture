87ceab2816d0a23c1c8628eb3aab553a735e6855
Dionysis Zindros <dionyziz@gmail.com>
2016-03-03 13:12:21 +0200

    Add defaults & required fields to sampleset model
    This patch adds the necessary new fields to sampleset which will let us
    keep track of what is being collected.
    
    Specifically:
    * The raw data captured from the network
    * The known alphabet vector so far
    * The alphabet vector being tested
    
    Some defaults are also added for fields that can have them.

--- backend/breach/models.py
+++ backend/breach/models.py
@@ -24,12 +24,56 @@ class SampleSet(models.Model):
     """
     victim = models.ForeignKey(Victim)
     # number of samples contained in the set
-    amount = models.IntegerField(default=1, help_text="Number of samples contained in the sampleset")
-    knownsecret = models.CharField(max_length=255, help_text="Known secret before the sample set was collected")
+    amount = models.IntegerField(
+        default=1,
+        help_text="Number of samples contained in the sampleset"
+    )
+
+    # sampleset knownstate: knownsecret and knownalphabet
+    knownsecret = models.CharField(
+        default="",
+        max_length=255,
+        help_text="Known secret before the sample set was collected"
+    )
     # assert(self.knownsecret[0:len(self.victim.target.prefix)] == self.victim.target.prefix)
-    started = models.DateTimeField(default=timezone.now, help_text="Date and time at which sample set collection was started")
-    completed = models.DateTimeField(help_text="When we stopped collecting samples for this sampleset, successfully or not")
-    success = models.BooleanField(help_text="Whether the samples in this sampleset were all collected successfully")
+    knownalphabet = models.CharField(
+        max_length=255,
+        help_text="The candidate alphabet for the next unknown character"
+    )
+    # assert(all([c in self.victim.target.alphabet for c in self.knownalphabet]))
+
+    # candidate state
+    candidatealphabet = models.CharField(
+        max_length=255,
+        help_text=("The alphabet vector that is attempted in this particular "
+                   "sample set."
+                   "When performing a divide and conquer attack, this will "
+                   "typically be a subset of the knownnextalphabet. In an "
+                   "iterative attack, it will be a single symbol of "
+                   "knownnextalphabet.")
+    )
+    # assert(all([c in self.knownalphabet for c in self.candidatealphabet]))
+
+    data = models.TextField(
+        default="",
+        help_text="The raw data collected on the network for this sampleset"
+    )
+
+    started = models.DateTimeField(
+        default=timezone.now,
+        help_text="Date and time at which sample set collection was started"
+    )
+    completed = models.DateTimeField(
+        null=True,
+        blank=True,
+        help_text=("When we stopped collecting samples for this sampleset, "
+                   "successfully or not")
+    )
+    success = models.BooleanField(
+        default=False,
+        help_text=("Whether the samples in this sampleset were all collected "
+                  "successfully")
+    )
 
 class AttackVectorElement(models.Model):
     sampleset = models.ForeignKey(SampleSet)
