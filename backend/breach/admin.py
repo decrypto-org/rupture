1f366622187de0580da90f9c38fdeb15b6a33ba0
Dionysis Zindros <dionyziz@gmail.com>
2016-03-05 14:09:59 +0200

    Remove unused model AttackVectorElement

--- backend/breach/admin.py
+++ backend/breach/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
-from .models import Target, Victim, SampleSet, AttackVectorElement
+from .models import Target, Victim, SampleSet
 
 admin.site.register(Target)
 admin.site.register(Victim)
 admin.site.register(SampleSet)
-admin.site.register(AttackVectorElement)
