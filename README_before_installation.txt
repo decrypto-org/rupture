178afbc85864beff4b82fbfd864c85af2ce6239f
Eva Sarafianou <eva.sarafianou@gmail.com>
2016-03-08 18:16:31 +0200

    adding installation scripts for all components

---
+++ README_before_installation.txt
@@ -0,0 +1,6 @@
+installClient.sh ,  installRealtime.sh 
+and installBackend.sh should be in /rupture
+
+before running one of those, 
+gain permission to run as user 
+(example: chmod +x installClient.sh)
---
+++ installBackend.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#PYTHON BACKEND INSTALLATION INSTRACTIONS FOR DEBIAN, UBUNTU & LINUXMINT
+
+
+
+#install python
+apt-get install -y python2.7
+
+#install virtual enviroment module using pip.
+apt-get install -y python-pip
+pip install virtualenv
+cd backend
+virtualenv env #create Python virtual environment for the project
+
+#Activate virtual environment
+source env/bin/activate
+
+#Install package dependencies
+pip install -r requirements.txt
+
+#Migrate database
+python manage.py migrate
+
+#Django project is ready to run!
