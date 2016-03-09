95f07ac5098a771de64f25383b501a2d0b952373
Dionysis Zindros <dionyziz@gmail.com>
2016-03-09 20:12:08 +0200

    Change django views to call strategy

--- backend/breach/views.py
+++ backend/breach/views.py
@@ -1,19 +1,35 @@
 import json
-from django.http import HttpResponse
+from django.http import Http404, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from breach.strategy import Strategy
+from breach.models import Victim
 
 
-def create_new_work():
-    return {'url': 'https://www.dimkarakostas.com/?breach-test',
-            'amount': 10,
-            'timeout': 0}
+def get_work(request, victim_id):
+    try:
+        victim = Victim.objects.get(pk=victim_id)
+    except:
+        raise Http404('Victim not found')
 
+    strategy = Strategy(victim)
 
-def get_work(request):
-    new_work = create_new_work()
+    # Example work structure:
+    # return {'url': 'https://www.dimkarakostas.com/?breach-test',
+    #         'amount': 10,
+    #         'timeout': 0}
+
+    new_work = strategy.get_work()
+
     return HttpResponse(json.dumps(new_work), content_type='application/json')
 
-
 @csrf_exempt
-def work_completed(request):
-    return HttpResponse('Not implemented')
+def work_completed(request, victim_id):
+    try:
+        victim = Victim.objects.get(pk=victim_id)
+    except:
+        raise Http404('Victim not found')
+
+    strategy = Strategy(victim)
+    strategy.work_completed()
+
+    return JsonResponse()
