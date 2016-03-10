3635a75680cb40be4ab3ae0eac24d62520570e9e
Dimitrios Karakostas <dimit.karakostas@gmail.com>
2016-03-10 20:49:34 +0200

    Fix options initialization

--- realtime/index.js
+++ realtime/index.js
@@ -12,19 +12,20 @@ winston.info('Listening on port ' + PORT);
 
 var socket = io.listen(PORT);
 
-const options = {
-    host: 'localhost',
-    port: '8000'
-};
+const BACKEND_HOST = 'localhost',
+      BACKEND_PORT = '8000';
 
 socket.on('connection', function(client) {
     winston.info('New connection from client ' + client.id);
 
     client.on('get-work', function() {
         winston.info('get-work from client ' + client.id);
+        var getWorkOptions = {
+            host: BACKEND_HOST,
+            port: BACKEND_PORT,
+            path: '/breach/get_work'
+        };
 
-        var getWorkOptions = options;
-        getWorkOptions['path'] = '/breach/get_work';
         http.request(getWorkOptions, function(response) {
             var res = '';
             response.on('data', function(chunk) {
@@ -44,13 +45,16 @@ socket.on('connection', function(client) {
     client.on('work-completed', function({work, success, host}) {
         winston.info('Client indicates work completed: ', work, success, host);
 
-        var workCompletedOptions = options;
-        workCompletedOptions['path'] = '/breach/work_completed';
-        workCompletedOptions['method'] = 'POST';
-
         var requestBody = work;
         requestBody['success'] = success;
-        workCompletedOptions['json'] = requestBody;
+
+        var workCompletedOptions = {
+            host: BACKEND_HOST,
+            port: BACKEND_PORT,
+            path: '/breach/work_completed',
+            method: 'POST',
+            json: requestBody
+        };
 
         http.request(workCompletedOptions, function(response) {
             var res = '';
