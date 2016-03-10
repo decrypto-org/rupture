aa10d2c0d49fd28afcda2b67f969fdb4a1d3072b
Dimitrios Karakostas <dimit.karakostas@gmail.com>
2016-03-10 20:51:06 +0200

    Change get_work to response with work JSON

--- backend/breach/views.py
+++ backend/breach/views.py
@@ -1,9 +1,17 @@
+import json
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 
+def create_new_work():
+    return {'url': 'https://www.dimkarakostas.com/?breach-test',
+            'amount': 10,
+            'timeout': 0}
+
+
 def get_work(request):
-    return HttpResponse('Not implemented')
+    new_work = create_new_work()
+    return HttpResponse(json.dumps(new_work), content_type='application/json')
 
 
 @csrf_exempt
