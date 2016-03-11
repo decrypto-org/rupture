b719f27c5b96f5f9393e75c7812a24630e5cf489
Eva Sarafianou <eva.sarafianou@gmail.com>
2016-03-11 11:02:41 +0200

    added check for apt-get command in all component's install scirpts,added comment for apt-get, and  deleted ed installation from client installation.

--- backend/install_backend.sh
+++ backend/install_backend.sh
@@ -1,24 +1,28 @@
 #!/bin/sh
 
-#PYTHON BACKEND INSTALLATION INSTRUCTIONS FOR DEBIAN, UBUNTU & LINUXMINT
+##PYTHON BACKEND INSTALLATION INSTRUCTIONS FOR DEBIAN, UBUNTU & LINUXMINT
+##check if apt-get command exists. if not, abort.
+command -v apt-get >/dev/null 2>&1 || 
+{ echo >&2 "Installation script requires  apt-get but it's not installed.  Aborting."; 
+exit 1; }
 
 
 
-#install python (execute with elevated privileges)
+##install python (execute with elevated privileges)
 sudo apt-get install -y python2.7
 
-#install virtual enviroment module using pip.
+##install virtual enviroment module using pip.
 sudo apt-get install -y python-pip
 pip install virtualenv
 virtualenv env #create Python virtual environment for the project
 
-#Activate virtual environment
+##Activate virtual environment
 source env/bin/activate
 
-#Install package dependencies
+##Install package dependencies
 pip install -r requirements.txt
 
-#Migrate database
+##Migrate database
 python manage.py migrate
 
-#Django project is ready to run!
+##Django project is ready to run!
--- client/install_client.sh
+++ client/install_client.sh
@@ -1,28 +1,30 @@
 #!/bin/sh
 
-#CLIENT INSTALLATION INSTRUCTIONS FOR DEBIAN, UBUNTU & LINUXMINT
+##CLIENT INSTALLATION INSTRUCTIONS FOR DEBIAN, UBUNTU & LINUXMINT
+##check if apt-get command is installed
+command -v apt-get >/dev/null 2>&1 || 
+{ echo >&2 "Installation script requires  apt-get but it's not installed.  Aborting."; 
+exit 1; }
 
-
-# installing nodejs 
-# source https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-an-ubuntu-14-04-server
+## installing nodejs 
+## source https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-an-ubuntu-14-04-server
 apt-get install -y nodejs npm
-npm install -g npm #update npm if already installed
+npm install -g npm ##update npm if already installed
 
-npm install -g gulp #install gulp on your system
+npm install -g gulp ##install gulp on your system
 
-#install required packages for compilation of the client code
+##install required packages for compilation of the client code
 npm install
 
-#bettercap installation (requires rubygem)
+##bettercap installation (requires rubygem)
 
-#install rubygems, a package manager for Ruby
-#source: http://www.cyberciti.biz/faq/debian-ubuntu-install-gem-the-frontend-to-rubygems/
+##install rubygems, a package manager for Ruby
+##source: http://www.cyberciti.biz/faq/debian-ubuntu-install-gem-the-frontend-to-rubygems/
 apt-get install rubygems build-essential
 gem install syslog-logger #install syslog-logger from local directory or remote server
 apt-get install build-essential ruby-dev libpcap-dev #install dependecies
 
-#install bettercap
+##install bettercap
 gem install bettercap
 
-#install ed gnu editor
-apt-get install ed
+
