256fbb1eb12a596ca3ae656a47398e44028793a7
Dimitrios Karakostas <dimit.karakostas@gmail.com>
2016-03-12 02:38:43 +0200

    Implement delete

--- sniffer/sniff.py
+++ sniffer/sniff.py
@@ -64,14 +64,48 @@ def start():
     return msg, 201
 
 
-@app.route('/get_capture')
 def get_capture():
     return 'Not implemented', 200
 
 
-@app.route('/delete_sniffer')
-def delete_sniffer():
-    return 'Not implemented', 200
+@app.route('/delete', methods=['POST'])
+def delete():
+    '''
+    Stop a sniffer from capturing, wait for the thread to finish and delete the sniffer.
+
+    Arguments:
+    source_ip -- the local network IP of the victim, e.g. 192.168.1.66
+    destination_host -- the hostname of the attacked endpoint, e.g. dimkarakostas.com
+
+    Status code for the request:
+            404: no sniffer exists with the given parameters
+            200: sniffer was deleted successfully
+    '''
+    data = request.get_json()
+    source_ip = data['source_ip']
+    destination_host = data['destination_host']
+
+    logger.debug('Deleting sniffer (source_ip : {}, destination_host: {})...'.format(source_ip, destination_host))
+
+    # Get the sniffer object and its source_ip and destination_host, if exists
+    try:
+        sniffer = sniffers[(source_ip, destination_host)]
+    except KeyError:
+        msg = '(get_sniff) 404 Not found: Sniffer (source_ip : {}, destination_host: {})'.format(source_ip, destination_host)
+        logger.warning(msg)
+        return msg, 404
+
+    # Stop the sniffer capture and wait for the thread to join
+    sniffer.stop()
+    sniffer.join()
+
+    # Delete the sniffer entries from both dictionaries
+    del sniffers[(source_ip, destination_host)]
+
+    msg = '(get_sniff) Sniffer (source_ip : {}, destination_host: {}) was deleted.'.format(source_ip, destination_host)
+    logger.debug(msg)
+
+    return msg, 200
 
 
 if __name__ == '__main__':
