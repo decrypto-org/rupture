919a04725ebfc27662b1b2b47ee24d0ebcc474c1
Dimitrios Karakostas <dimit.karakostas@gmail.com>
2016-03-12 02:29:46 +0200

    Add Sniffer parameter initialization

--- sniffer/sniffer.py
+++ sniffer/sniffer.py
@@ -1,14 +1,31 @@
 import threading
-from time import sleep
+import socket
 
 
 class Sniffer(threading.Thread):
     def __init__(self, arg):
+        super(Sniffer, self).__init__()
+
         # Set thread to run as daemon
         self.daemon = True
 
         # Initialize object variables with parameters from arg dictionary
         self.arg = arg
+        try:
+            self.interface = arg['interface']
+            self.source_ip = arg['source_ip']
+            self.destination_host = arg['destination_host']
+        except KeyError:
+            assert False, 'Invalid argument dictionary - Not enough parameters'
+
+        try:
+            self.destination_ip = socket.gethostbyaddr(self.destination_host)[-1][0]
+        except socket.herror, err:
+            assert False, 'socket.herror - ' + str(err)
+
+        # If either of the parameters is None, assert error
+        assert self.interface and self.source_ip and self.destination_host, 'Invalid argument dictionary - Invalid parameters'
+
 
         # Thread has not come to life yet
         self.status = False
