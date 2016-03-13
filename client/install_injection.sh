a1f3d45551f272c301a2bae9d48e915d7ba3e269
Eva Sarafianou <eva.sarafianou@gmail.com>
2016-03-13 12:34:28 +0200

    changed file permissions

--- client/install_injection.sh
+++ client/install_injection.sh
@@ -4,14 +4,10 @@ command -v apt-get >/dev/null 2>&1 ||
 { echo >&2 "Installation script requires  apt-get but it's not installed.  Aborting.";
 exit 1; }
 
+#bettercap installation (requires rubygem)
 
-##bettercap installation (requires rubygem)
-
-##install rubygems, a package manager for Ruby
-##source: http://www.cyberciti.biz/faq/debian-ubuntu-install-gem-the-frontend-to-rubygems/
 apt-get install rubygems build-essential
 gem install syslog-logger #install syslog-logger from local directory or remote server
 apt-get install build-essential ruby-dev libpcap-dev #install dependencies
 
 gem install bettercap
-
