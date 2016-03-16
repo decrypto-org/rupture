486c0dfc942f43cb1cf39c4b7e06dda5bb91d803
Dimitrios Karakostas <dimit.karakostas@gmail.com>
2016-03-16 03:24:04 +0200

    Add balance checking test

--- backend/breach/tests/test_strategy.py
+++ backend/breach/tests/test_strategy.py
@@ -33,3 +33,19 @@ class StrategyTestCase(RuptureTestCase):
 
     def test_advance_round(self):
         pass
+
+    @patch('breach.strategy.Sniffer')
+    def test_alphabet_balance(self, Sniffer):
+        strategy_0 = Strategy(self.balance_victim)
+        work_0 = strategy_0.get_work()
+        self.assertEqual(
+            work_0['url'],
+            'https://di.uoa.gr/?breach=^testsecret0^testsecret$^testsecret(^1^3^2^'
+        )
+
+        strategy_1 = Strategy(self.balance_victim)
+        work_1 = strategy_1.get_work()
+        self.assertEqual(
+            work_1['url'],
+            'https://di.uoa.gr/?breach=^testsecret3^testsecret2^testsecret1^0^$^(^'
+        )
