6e3151cd9e4c5309959c93b2ed683bb74d88a640
Dionysis Zindros <dionyziz@gmail.com>
2016-07-11 17:20:30 +0300

    Migrate Sniffer unit test to new API

--- backend/breach/tests/test_sniffer.py
+++ backend/breach/tests/test_sniffer.py
@@ -7,22 +7,19 @@ from breach.sniffer import Sniffer
 class SnifferTest(TestCase):
     def setUp(self):
         self.endpoint = 'http://localhost'
-        self.sniffer = Sniffer(self.endpoint)
-
-        self.source_ip = '147.102.239.229'
-        self.destination_host = 'dionyziz.com'
+        self.sniffer = Sniffer(self.endpoint, '147.102.239.229', 'dionyziz.com', 'wlan0', '8080')
 
     @patch('breach.sniffer.requests')
     def test_sniffer_start(self, requests):
-        self.sniffer.start(self.source_ip, self.destination_host)
+        self.sniffer.start()
         self.assertTrue(requests.post.called)
 
     @patch('breach.sniffer.requests')
     def test_sniffer_read(self, requests):
-        self.sniffer.read(self.source_ip, self.destination_host)
+        self.sniffer.read()
         self.assertTrue(requests.get.called)
 
     @patch('breach.sniffer.requests')
     def test_sniffer_delete(self, requests):
-        self.sniffer.delete(self.source_ip, self.destination_host)
+        self.sniffer.delete()
         self.assertTrue(requests.post.called)
