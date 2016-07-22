05d940809b0aed7f6351e8215c1da45bb4cbdbd9
Dionysis Zindros <dionyziz@gmail.com>
2016-07-22 14:49:19 +0300

    Add webpack/babel compiler

---
+++ client/webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
+};
