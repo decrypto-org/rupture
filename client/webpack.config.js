89dbe1f3d9198510f1f8b9e13654f6dc75fb224d
Dionysis Zindros <dionyziz@gmail.com>
2016-07-31 02:43:10 +0300

    Add babel/webpack transpilation caching

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -14,7 +14,8 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: 'babel-loader',
                 query: {
-                    presets: ['es2015']
+                    presets: ['es2015'],
+                    cacheDirectory: true
                 }
             }
         ]
