aa04e34de7d8028f249498bfa30cdd66189968b0
Dionysis Zindros <dionyziz@gmail.com>
2016-07-31 16:02:36 +0300

    Realtime service end-to-end tests

---
+++ realtime/spec/realtimespec.js
@@ -0,0 +1,124 @@
+const spawn = require('child_process').spawn;
+const client = require('socket.io-client');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+function runServer() {
+    server = spawn(
+        'node',
+        [
+            '--harmony',
+            'index.js',
+            '--port', '9909',
+            '--backend-host', 'localhost',
+            '--backend-port', '9908'
+        ]
+    );
+
+    server.stdout.on('data', (data) => {
+        console.log(data.toString());
+    });
+    server.stderr.on('data', (data) => {
+        console.log(data.toString());
+    });
+    server.on('exit', () => {
+        console.log('Server terminated');
+    });
+
+    return server;
+}
+
+function connect() {
+    const socket = client('http://localhost:9909');
+
+    socket.on('error', () => {});
+
+    return socket;
+}
+
+describe('real-time service', () => {
+    let server,
+        socket;
+
+    beforeEach(() => {
+        server = runServer();
+        socket = connect();
+    });
+
+    it('listens for websocket connections', (done) => {
+        socket.on('connect', done);
+    });
+
+    it('exchanges hello messages', function(done) {
+        socket.on('connect', () => {
+            socket.emit('client-hello', {victim_id: 5});
+            socket.on('server-hello', done);
+        });
+    });
+
+    describe('backend communication', () => {
+        let fakeServer;
+
+        function fakeBackend(listenCallback, method, url, getCallback) {
+            const app = express();
+            app.use(bodyParser.json());
+            app[method](url, getCallback);
+            fakeServer = app.listen(9908, listenCallback);
+        }
+
+        beforeEach((done) => {
+            socket.on('connect', () => {
+                socket.emit('client-hello', {victim_id: 5});
+                done();
+            });
+        });
+
+        it('requests work from the backend', function(done) {
+            fakeBackend(
+                () => {
+                    socket.emit('get-work');
+                },
+                'get',
+                '/breach/get_work/5',
+                done
+            );
+        });
+
+        it('reports work-completed to the backend', function(done) {
+            fakeBackend(
+                () => {
+                    socket.emit('work-completed', {work: {}, success: true});
+                },
+                'post',
+                '/breach/work_completed/5',
+                (request, response) => {
+                    expect(request.body.success).toBe(true);
+                    done();
+                }
+            );
+        });
+
+        it('reports failed work-completed after a client disconnects', function(done) {
+            fakeBackend(
+                () => {
+                    socket.close();
+                },
+                'post',
+                '/breach/work_completed/5',
+                (request, response) => {
+                    expect(request.body.success).toBe(false);
+                    done();
+                }
+            );
+        });
+
+        afterEach(() => {
+            fakeServer.close();
+        });
+    });
+
+    afterEach(() => {
+        server.kill();
+        socket.close();
+    });
+});
