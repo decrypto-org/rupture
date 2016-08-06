8662702cd27a181d65ce9c4ba05a024efd90c28e
Dimitris Karakostas <dimit.karakostas@gmail.com>
2016-08-07 00:08:25 +0300

    Add ~/.rupture check in setup_backend

--- backend/setup_backend.sh
+++ backend/setup_backend.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
 BASEDIR=$(dirname -- "$(readlink -f -- "${BASH_SOURCE}")")
+RUPTUREDIR="$HOME/.rupture"
 
 echo '[-] Flushing old database, if exists...'
 $BASEDIR/env/bin/python $BASEDIR/manage.py flush --no-input
+
+if [ -d $RUPTUREDIR ]; then
+    echo "[-] Using Rupture directory in $RUPTUREDIR..."
+    mkdir -p $RUPTUREDIR/backend
+
+    echo "[-] Copying configuration files to config directory..."
+    cp $BASEDIR/victim_config.yml $RUPTUREDIR/victim_config.yml
+    cp $BASEDIR/target_config.yml $RUPTUREDIR/target_config.yml
+else
+    RUPTUREDIR=$BASEDIR
+fi
 
 echo '[-] Applying Django migrations...'
 if $BASEDIR/env/bin/python $BASEDIR/manage.py migrate; then
