e099b9b705fa65fdf33dc1b847ebd0a722e3d0c2
Dimitris Karakostas <dimit.karakostas@gmail.com>
2016-08-06 23:52:53 +0300

    Add user message when running bettercap with sudo

--- client/inject.sh
+++ client/inject.sh
@@ -6,4 +6,6 @@ SOURCEIP=$1
 
 iconv -f utf-8 -t ascii//TRANSLIT $BASEDIR/dist/main.js > $BASEDIR/dist/main2.js
 mv -f $BASEDIR/dist/main2.js $BASEDIR/dist/main.js
+
+echo "[*] Bettercap needs sudo privileges. Please enter your user password."
 sudo bettercap -T ${SOURCEIP} --proxy --proxy-module injectjs --js-file $BASEDIR/dist/main.js
