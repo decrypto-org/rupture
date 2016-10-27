786e73346bb4dd586c4e0394ef2896905e3f7e2d
Eva Sarafianou <eva.sarafianou@gmail.com>
2016-10-27 18:24:10 +0300

    Add /target to urls

--- backend/breach/urls.py
+++ backend/breach/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^get_work/(?P<victim_id>\d+)$', views.get_work, name='get_work'),
     url(r'^get_work$', views.get_work, name='get_work'),
     url(r'^work_completed/(?P<victim_id>\d+)$', views.work_completed, name='work_completed'),
-    url(r'^work_completed$', views.work_completed, name='work_completed')
+    url(r'^work_completed$', views.work_completed, name='work_completed'),
+    url(r'^target$', views.TargetView.as_view(), name='TargetView')
 ]
