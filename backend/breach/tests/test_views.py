9e54342090b586dbd622a9fd57fdb15f8f5c5227
Eva Sarafianou <eva.sarafianou@gmail.com>
2016-12-08 22:38:06 +0200

    Add test for /target get

--- backend/breach/tests/test_views.py
+++ backend/breach/tests/test_views.py
@@ -72,3 +72,12 @@ class ViewsTestCase(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(json.loads(response.content)['target_name'], 'ruptureit3')
+
+    def test_target_get(self):
+
+        response = self.client.get(reverse('TargetView'))
+        response_dict1 = {key: json.loads(response.content)['targets'][0][key] for key in self.target1_data}
+        response_dict2 = {key: json.loads(response.content)['targets'][1][key] for key in self.target2_data}
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response_dict1, self.target1_data)
+        self.assertEqual(response_dict2, self.target2_data)
