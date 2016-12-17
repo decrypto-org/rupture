9f7f6b329229811e959274a666a81895c2d66153
Eva Sarafianou <eva.sarafianou@gmail.com>
2016-12-17 20:10:28 +0200

    Make /victim/notstarted GET request when wifiscan button is clicked

--- frontend/src/containers/Wifiscan.js
+++ frontend/src/containers/Wifiscan.js
@@ -7,15 +7,31 @@ export default class WifiScan extends React.Component {
     constructor() {
         super();
         this.state = { 
-            message: <p>Scan for victims</p> 
+            scanning: false
         };
     }
 
+    handleClick = () => {
+        this.setState({ scanning: true });
+
+        axios.get('/breach/victim/notstarted')
+        .then(res => {
+            let victims = res.data['new_victims'];
+            this.props.onUpdate(victims);
+            this.setState({ scanning: false })
+        })
+        .catch(error => {
+            console.log(error);
+            this.setState({ scanning: false })
+        });
+    } 
+                
     render(){
         return(
             <div>
-                <input type='image' src={WifiIcon} className='nooutline' name='wifi scan' onClick={ this.handleClick }/>
-                { this.state.message }
+                <img type='image' src={WifiIcon} className='nooutline' alt='wifi button'
+                    name='wifi scan' onClick={ this.handleClick }/>
+                { this.state.scanning ? <p>Scanning...</p> : <p>Scan for victims</p> }
             </div>
         );
     }
