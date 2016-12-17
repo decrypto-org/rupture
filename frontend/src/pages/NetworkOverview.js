b7c6e8714026a8f01b551fdfbc0e8145f7521641
Eva Sarafianou <eva.sarafianou@gmail.com>
2016-12-17 16:07:13 +0200

    Add the NetworkOverview page

---
+++ frontend/src/pages/NetworkOverview.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router';
+import axios from 'axios';
+import _ from 'lodash.partition';
+
+import AttackListDetails from '../containers/AttackListDetails';
+import NotStartedVictims from '../containers/NotStartedVictims';
+import WifiScan from '../containers/Wifiscan';
+
+import GhostPc from '../img/ghost_pc.png';
+
+export default class NetworkOverview extends React.Component {
+
+    constructor() {
+        super();
+        this.state = { attacks: [], victims: [] };
+    }
+
+    onScan = (victims) => {
+        this.setState({ victims: victims });
+    }
+
+    render() {
+        return(
+            <div>
+                <div className='container-fluid'>
+                    <h1> Network Overview </h1>
+                    <div className='row'>
+                        <div id='mainpage' className='col-md-8 col-xs-12 col-sm-6 col-lg-8'>
+                            { this.state.attacks.length > 0 ? <AttackListDetails attacks={ this.state.attacks } onReload={ this.getVictims }/> : null}
+                            { this.state.victims.length > 0 ? <NotStartedVictims victims={ this.state.victims }/> : null}
+                        </div>
+                        <div className='button col-md-4 col-xs-6 col-lg-4'>
+                            <WifiScan onUpdate={ this.onScan }/>
+                            <div className='ghost'>
+                                <Link to='attackconfig'>
+                                    <img src={GhostPc} alt='A Ghost PC' title='Add a custom victim' className='nooutline'/>
+                                    <span className='line leftpadding'>Add custom victim</span>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
