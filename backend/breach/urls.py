aee27789a559b4d2c17d359bb39682e21474b13f
Eva Sarafianou <eva.sarafianou@gmail.com>
2016-12-18 13:45:35 +0200

    Add /attack to urls

--- backend/breach/urls.py
+++ backend/breach/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^work_completed/(?P<victim_id>\d+)$', views.work_completed, name='work_completed'),
     url(r'^work_completed$', views.work_completed, name='work_completed'),
     url(r'^target$', views.TargetView.as_view(), name='TargetView'),
-    url(r'^victim$', views.VictimListView.as_view(), name='VictimListView')
+    url(r'^victim$', views.VictimListView.as_view(), name='VictimListView'),
+    url(r'^attack$', views.AttackView.as_view(), name='AttackView')
 ]
