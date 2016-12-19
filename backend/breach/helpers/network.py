629fab227cb5d564d6cb7d9469c76915eb6c72ac
Eva Sarafianou <eva.sarafianou@gmail.com>
2016-12-19 13:02:35 +0200

    Add function to get the machine's local IP

--- backend/breach/helpers/network.py
+++ backend/breach/helpers/network.py
@@ -3,3 +3,8 @@ import netifaces
 
 def get_interface():
     return netifaces.gateways()['default'][netifaces.AF_INET][1]
+
+
+def get_local_IP():
+    def_gw_device = get_interface()
+    return netifaces.ifaddresses(def_gw_device)[netifaces.AF_INET][0]['addr']
