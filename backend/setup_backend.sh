95a0ae37f6979ece4fedce081ff5d3b3e5b472c3
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-02-01 21:24:32 +0200

    Flush backend db instead of deleting&migrating

--- backend/setup_backend.sh
+++ backend/setup_backend.sh
@@ -2,12 +2,8 @@
 
 BASEDIR=$(dirname "$0")
 
-echo '[-] Removing old database, if exists...'
-rm -f $BASEDIR/db.sqlite3
-if test -e "$BASEDIR/db.sqlite3"; then
-    echo "[!] Database could not be deleted. Check permissions on $BASEDIR/db.sqlite3?";
-    exit;
-fi
+echo '[-] Flushing old database, if exists...'
+$BASEDIR/env/bin/python $BASEDIR/manage.py flush --no-input
 
 echo '[-] Applying Django migrations...'
 if $BASEDIR/env/bin/python $BASEDIR/manage.py migrate; then
