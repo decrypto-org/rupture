7a279be908e9010059ad464948abeb64bda3a749
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-05-15 17:49:03 +0300

    Use matplotlib for ctx experiment figure

--- etc/theory/defense_experiments/experiment.py
+++ etc/theory/defense_experiments/experiment.py
@@ -1,5 +1,7 @@
 from ctx_defense import CTX
 import gzip
+import matplotlib.pyplot as plt
+from collections import OrderedDict
 
 with open('social_network_script', 'r') as f:
     text = f.read()
@@ -34,6 +36,7 @@ for i in range(50, 101):
     with gzip.open(ctxed_filename.format(i), 'wb') as f:
         f.write(plaintext)
 
+formed_output = {'reflection': [], 'ideal': [], 'ctx': []}
 print 'Reflection\tIdeal security\tCTX'
 for i in range(50, 101):
     with open(plain_filename.format(i), 'r') as f:
@@ -42,3 +45,33 @@ for i in range(50, 101):
             ideal_length = len(f.read())
             ctx_length = len(g.read())
             print '{}\t\t{}\t\t{}'.format(reflection_length, ideal_length, ctx_length)
+            formed_output['reflection'].append(reflection_length)
+            formed_output['ideal'].append(ideal_length)
+            formed_output['ctx'].append(ctx_length)
+
+ref = formed_output['reflection']
+ideal = formed_output['ideal']
+ctx = formed_output['ctx']
+res = OrderedDict([
+    ('Ideal', ideal),
+    ('CTX', ctx)
+])
+
+font = {
+    'size': 12
+}
+
+plt.rc('font', **font)
+
+plt.title('CTX & Ideal security length comparison', y=1.01)
+
+plt.ylabel('Compressed (Bytes)')
+plt.xlabel('Reflection (Bytes)')
+for i in res:
+    plt.plot(ref, res[i])
+
+plt.legend([i for i in res])
+plt.ylim(ymin=0)
+plt.xlim(xmin=50)
+
+plt.savefig('ctx_experiment.png')
