22840d46aab90dd29ca820fa6d4dd2b74b1ad228
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-12-07 13:32:42 +0000

    Add backtracking probabilities calculation unit test

--- backend/breach/tests/test_backtracking.py
+++ backend/breach/tests/test_backtracking.py
@@ -90,3 +90,99 @@ class BacktrackingTestCase(RuptureTestCase):
         self.assertEqual(decision[0]['knownalphabet'], '0123456789')
         self.assertEqual(decision[1]['knownsecret'], 'testsecret0')
         self.assertEqual(decision[1]['knownalphabet'], '0123456789')
+
+    @patch('breach.strategy.Sniffer')
+    def test_backtracking_probabilities(self, Sniffer):
+        capture0 = {'data': 30 * '0', 'records': 1}
+        capture1 = {'data': 32 * '1', 'records': 1}
+        capture2 = {'data': 34 * '0', 'records': 1}
+        capture3 = {'data': 31 * '1', 'records': 1}
+        sniffer_instance = Sniffer.return_value
+
+        BACKTRACKING = 3
+
+        # Mock initial round
+        target = Target.objects.create(
+            endpoint='https://test.test/?breach=%s',
+            prefix='pref',
+            alphabet='01',
+            name='test',
+            secretlength=6,
+            method=BACKTRACKING,
+            compression_function_factor=1.05,
+            amplification_factor=10.0
+        )
+
+        victim = Victim.objects.create(
+            target=target,
+            sourceip='0.0.0.0',
+            snifferendpoint='http://localhost/'
+        )
+
+        sniffer_instance.read.return_value = capture0
+        strategy = Strategy(victim)
+        strategy.get_work()
+        strategy = Strategy(victim)
+        strategy.work_completed()
+
+        sniffer_instance.read.return_value = capture1
+        strategy = Strategy(victim)
+        strategy.get_work()
+        strategy = Strategy(victim)
+        strategy.work_completed()
+
+        scoreboard = strategy.get_backtracking_scoreboard()
+        self.assertEqual(
+            scoreboard[0]['knownsecret'],
+            'pref0'
+        )
+        self.assertEqual(
+            scoreboard[1]['knownsecret'],
+            'pref1'
+        )
+        self.assertEqual(
+            scoreboard[0]['accumulated_probability'],
+            5.243757431629014
+        )
+        self.assertEqual(
+            scoreboard[1]['accumulated_probability'],
+            4.756242568370987
+        )
+
+        sniffer_instance.read.return_value = capture2
+        strategy = Strategy(victim)
+        strategy.get_work()
+        strategy = Strategy(victim)
+        strategy.work_completed()
+
+        sniffer_instance.read.return_value = capture3
+        strategy = Strategy(victim)
+        strategy.get_work()
+        strategy = Strategy(victim)
+        strategy.work_completed()
+
+        scoreboard = strategy.get_backtracking_scoreboard()
+        self.assertEqual(
+            scoreboard[0]['knownsecret'],
+            'pref01'
+        )
+        self.assertEqual(
+            scoreboard[1]['knownsecret'],
+            'pref00'
+        )
+        self.assertEqual(
+            scoreboard[2]['knownsecret'],
+            'pref1'
+        )
+        self.assertEqual(
+            scoreboard[0]['accumulated_probability'],
+            28.134197076830024
+        )
+        self.assertEqual(
+            scoreboard[1]['accumulated_probability'],
+            24.303377239460115
+        )
+        self.assertEqual(
+            scoreboard[2]['accumulated_probability'],
+            4.756242568370987
+        )
