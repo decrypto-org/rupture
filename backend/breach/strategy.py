c1e416394aaf3963c34803523c7663231bbb1f20
Dimitris Karakostas <dimit.karakostas@gmail.com>
2017-12-08 14:29:48 +0000

    Update also target recordscardinality if necessary

--- backend/breach/strategy.py
+++ backend/breach/strategy.py
@@ -493,6 +493,8 @@ class Strategy(object):
                             if self._need_for_cardinality_update():
                                 self._victim.recordscardinality = int(capture['records'] / self._victim.target.samplesize)
                                 self._victim.save()
+                                self._victim.target.recordscardinality = int(capture['records'] / self._victim.target.samplesize)
+                                self._victim.target.save()
                                 self._flush_batch_samplesets()
                                 logger.debug("Updating records' cardinality. New cardinality: {}".format(self._victim.recordscardinality))
 
